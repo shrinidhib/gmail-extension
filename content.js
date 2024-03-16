64ca25bb7ddc5a8bb6c06d1206fe682ba8e0c3b0
Shrinidhi <b.shrinidhi1214@gmail.com>
2024-03-16 15:21:31 +0530

    first commit

---
+++ content.js
@@ -0,0 +1,40 @@
+function categorise(){
+    const emails=document.querySelectorAll('.zA');
+    console.log('here')
+    emails.forEach(email=>{
+        const subject = email.querySelector('.bog span').textContent;
+        console.log(subject)
+        const emailBodyElement = document.querySelector('div.adP');
+        const emailId = emailBodyElement ? emailBodyElement.getAttribute('data-message-id') : null;
+
+        console.log(emailId)
+
+    
+    let category;
+    if (subject.includes('important')) {
+      category = 'Important';
+    } else if (subject.includes('promotion')) {
+      category = 'Promotion';
+    } else {
+      category = 'Other';
+    }
+
+    // Create and append category label
+    const categoryLabel = document.createElement('span');
+    categoryLabel.textContent = `[${category}]`;
+    categoryLabel.style.color = 'blue';
+    categoryLabel.style.marginLeft = '5px';
+    categoryLabel.classList=['new']
+    email.querySelector('.y6 span').appendChild(categoryLabel);
+    })
+}
+
+document.addEventListener('DOMContentLoaded', (e)=>{
+    console.log('here1')
+    categorise()});
+
+window.addEventListener('load', (event) => {
+        console.log('here2')
+        categorise()
+        // Call your content script injection function here
+    });
